7b48318ae5ac24fb522069b29d3c32eba62412fe
MyHomeMyData <juergen.bonfert@gmail,com>
2024-03-08 14:04:51 +0100

    Store numbers with type Number instead of String

--- main.js
+++ main.js
@@ -145,7 +145,7 @@ class E3oncan extends utils.Adapter {
             if (devDids.didsDevSpecAvail) {
                 if ( (devDids.didsDictDevCom.Version === undefined) ||
                     (Number(E3DidsDict.Version) > Number(devDids.didsDictDevCom.Version)) ) {
-                    this.log.debug('Updating datapoints to version '+E3DidsDict.Version+' for device '+dev.devStateName);
+                    this.log.info('Updating datapoints to version '+E3DidsDict.Version+' for device '+dev.devStateName);
                     for (const did of Object.keys(devDids.didsDictDevCom)) {
                         if ( (did != 'Version') &&  (did in E3DidsDict) ) {
                             // Check for changes in datapoint structure
@@ -154,18 +154,31 @@ class E3oncan extends utils.Adapter {
                             if (JSON.stringify(devStruct) != JSON.stringify(E3Struct)) {
                                 // Structure of datapoint has changed
                                 // Replace .json and .tree state(s) based on raw data of did
-                                const didStateName = await devDids.getDidStr(did)+'_'+devDids.didsDictDevCom[did].id;
-                                this.log.debug('  > Structure of datapoint '+didStateName+' has changed. Updating.');
+                                const didStateName = await devDids.getDidStr(did)+'_'+await devDids.didsDictDevCom[did].id;
+                                this.log.info('  > Structure of datapoint '+didStateName+' has changed. Updating.');
                                 // Delete states based on old structure:
                                 await this.delObjectAsync(this.namespace+'.'+dev.devStateName+'.tree.'+didStateName, { recursive: true });
                                 await this.delObjectAsync(this.namespace+'.'+dev.devStateName+'.json.'+didStateName, { recursive: true });
                                 const raw = await devDids.getObjectVal(this, dev.devStateName+'.raw.'+didStateName);
                                 if (raw != null) {
                                     // Create states based on new structure if raw data is available:
-                                    const cdi = E3DidsDict[did];
-                                    const res = await devDids.decodeDid(this, dev.devStateName, did, cdi, await devDids.arr2Hex(raw));
+                                    const cdi = await E3DidsDict[did];
+                                    const res = await devDids.decodeDid(this, dev.devStateName, did, cdi, devDids.toByteArray(raw));
                                     await devDids.storeObjectJson(this, did, res.idStr, this.namespace+'.'+dev.devStateName+'.json.'+didStateName, res.val);
                                     await devDids.storeObjectTree(this, did, res.idStr, this.namespace+'.'+dev.devStateName+'.tree.'+didStateName, res.val);
+                                }
+                            } else {
+                                // No change of Structure of datapoint
+                                // Make sure, data type and role of tree objects are correct
+                                // Forde update if .tree state(s) based on raw data of did
+                                const didStateName = await devDids.getDidStr(did)+'_'+await devDids.didsDictDevCom[did].id;
+                                const raw = await devDids.getObjectVal(this, dev.devStateName+'.raw.'+didStateName);
+                                if (raw != null) {
+                                    // Update .tree states:
+                                    this.log.silly('  > Update type and role of datapoint '+didStateName);
+                                    const cdi = await E3DidsDict[did];
+                                    const res = await devDids.decodeDid(this, dev.devStateName, did, cdi, devDids.toByteArray(raw));
+                                    await devDids.storeObjectTree(this, did, res.idStr, this.namespace+'.'+dev.devStateName+'.tree.'+didStateName, res.val, true);
                                 }
                             }
                             devDids.didsDictDevCom[did] = await E3DidsDict[did];
