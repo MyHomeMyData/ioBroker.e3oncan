2058710d069b7b160b178729674324f9e8199528
MyHomeMyData <juergen.bonfert@gmail,com>
2024-05-09 18:35:25 +0200

    Fix config for eslint 9.x

---
+++ eslint.config.mjs
@@ -0,0 +1,7 @@
+// eslint.config.js
+// See "Globally ignoring files with ignores" at https://eslint.org/docs/latest/use/configure/configuration-files
+export default [
+    {
+        ignores: ["**/.eslintrc.js", "admin/words.js"]
+    }
+];
